f3b50e725bf45919e8b63b21b6d20f1972882dbf
Meowcat285 <Meowcat285@users.noreply.github.com>
2018-10-24 13:18:51 -0700

    Add files via upload

---
+++ securly/main.js
@@ -0,0 +1,40 @@
+window.userStatus = {
+	NOTFOUND: -1,
+	FOUND: 1
+};
+
+window.clusterStatus = {
+	ERROR: -2,
+	NOTFOUND: -1,
+	FOUND: 1,
+	AVOID_OS: 2,
+	UNKNOWN_SCHOOL: 3
+};
+
+window.version = "-";
+window.userFound = window.userStatus.NOTFOUND;
+window.clusterFound = window.clusterStatus.NOTFOUND;
+window.userEmail = "notloggedin";
+window.clusterUrl = "unknown";
+window.ytpref = "prefnotchecked";
+window.ytprefnewvalue = "notset";
+
+// needToReloadTabs = 1 means that next time when clusterFound is FOUND, need to reload all tabs
+window.needToReloadTabs = 1;
+window.isBlockedYTVideo = false;
+// debugIWF = 0 is normal mode, 1 means disable IWF, 2 means force update IWF
+window.debugIWF = 0;
+window.IWFTimeout = 7 * 24 * 60 * 60 * 1000;
+
+getVersion();
+
+// setup the web request listener before fetchUser()
+// otherwise kids can have access to all sites as long as he doesn't click accept at the first time
+setupListener();
+fetchUserAPI();
+setupIWF();
+checkPnPHub();
+// request after 15 min for checking ip rule in pnphub
+setInterval(function(){
+	checkPnPHub();
+}, 900000);
